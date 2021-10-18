889d518e53a6100b9b20f050faf1a95a3f943c8d
Dominick Campanile <Dominick@Dominicks-MacBook-Air.local>
2021-10-18 12:37:02 -0500

    second commit

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Register')
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app
+from flask import render_template
+from app.forms import RegistrationForm
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html', name='Dominick')
 
-@app.route('/test')
-def name():
-    return 'Test123'
+@app.route('/register')
+def register():
+    register_form = RegistrationForm()
+    return render_template('register.html', form=register_form)
