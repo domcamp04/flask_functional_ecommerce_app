bb3fb58a6cb55176270d3714b978f65f902538f8
Dominick Campanile <Dominick@Dominicks-MacBook-Air.local>
2021-10-18 15:48:10 -0500

    added products table

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from enum import unique
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return f'<User|{self.username}>'
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Numeric(5,2), nullable=False)
+    image_url = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, name, price, image_url=None):
+        self.name = name
+        self.price = price
+        self.image_url = image_url
+
+    def __repr__(self):
+        return f'<Product | {self.name}>'
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,25 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, redirect, url_for
 from app.forms import RegistrationForm
+from app.models import User
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', name='Dominick')
+    return render_template('index.html', name='Brian')
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route('/register', methods=["GET", "POST"])
 def register():
     register_form = RegistrationForm()
     if register_form.validate_on_submit():
         username = register_form.username.data
         password = register_form.password.data
-        print(username, password)
+        # print(username, password)
+        new_user = User(username, password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
     return render_template('register.html', form=register_form)
